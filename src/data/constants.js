aa3d675db845ceb8e3cc1a5eb7395bbeb5d06712
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-23 18:18:05 -0400

    added screenMode

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,26 @@
 export const DEFAULT_LANG_CODE = 'en';
 
+export const screenMode = {
+    light: {
+        bg: 'bg-white',
+        sidebar: 'bg-gray-200',
+        text: 'text-dark',
+        border: 'border-white'
+    },
+    dark: {
+        bg: 'bg-slate-900',
+        sidebar: 'bg-gray-700',
+        text: 'text-white',
+        border: 'border-gray-700'
+    },
+    highlight: {
+        bg: 'bg-blue-500',
+        text: 'text-blue-500',
+        border: 'border-blue-500',
+        shadow: 'shadow-blue-500/50'
+    }
+}
+
 export const programming_languages = ['C', 'C++', 'Python'];
 export const web_technologies = ['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'NodeJs', 'ReactJs', 'ReduxJs', 'NextJs', 'Bootstrap', 'Tailwind CSS'];
 export const databases = ['MongoDB', 'Oracle','Microsoft Server'];
