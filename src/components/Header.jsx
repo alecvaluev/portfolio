e9ff686f60f21ce3c8af5cdf2cbdb91d9ec96a4e
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-23 18:15:00 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,23 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 import { selectDarkMode } from '../features/darkMode/darkModeSlice';
 import { HiOutlineDocumentDownload } from 'react-icons/hi';
+//import data
 import resume  from '../data/resume.pdf';
+import { screenMode } from '../data/constants';
 
 export default function Header({headerOpen}) {
     const [showHeader, setShowHearder] = useState(headerOpen);
     const language = useSelector(selectLanguage);
     const darkMode = useSelector(selectDarkMode);
 
-    const hoverStyle = 'hover:text-blue-500 pointer';
+    const hoverStyle = `hover:${screenMode.highlight.text} pointer`;
 
     const changeShowHeader = () =>{
         setShowHearder(!showHeader);
     }
 
     return (
-        <div className={`fixed ${darkMode? 'bg-slate-900 ':'bg-white'} md:bg-transparent w-full`}
+        <div className={`fixed ${darkMode? screenMode.dark.bg :screenMode.light.bg} md:bg-transparent w-full`}
              style={{zIndex: 9999}}>
             {/* toggle button */}
             <div className='block md:hidden m-2 ml-4 pt-3'
@@ -28,7 +30,7 @@ export default function Header({headerOpen}) {
                 {showHeader? <ImCancelCircle/> : <AiOutlineMenu/>}
             </div>
             {
-                showHeader && <div className={`flex flex-col md:flex-row md:justify-between ${darkMode? 'bg-slate-900 ':'bg-white'} md:ml-20 md:my-10 uppercase font-bold p-3 border-t border-b md:rounded-l-full shadow-xl`}>
+                showHeader && <div className={`flex flex-col md:flex-row md:justify-between ${darkMode? screenMode.dark.bg :screenMode.light.bg} md:ml-20 md:my-10 uppercase font-bold p-3 border-t border-b md:rounded-l-full shadow-xl`}>
                     {/* left part */}
                     <div className="pl-5 hidden md:block text-xl">
                         <a href="/" className={hoverStyle}>
@@ -47,7 +49,7 @@ export default function Header({headerOpen}) {
                         </ul>
                         {/* resume */}
                         <a href={resume} target='_blank' rel="noreferrer">
-                            <div className={`${hoverStyle} border-2 px-5 py-3 rounded-xl hover:border-blue-500 flex gap-1 text-center`}>{language.headline_resume} <span className='text-3xl'><HiOutlineDocumentDownload/></span></div>
+                            <div className={`${hoverStyle} border-2 px-5 py-3 rounded-xl hover:${screenMode.highlight.border} flex gap-1 text-center`}>{language.headline_resume} <span className='text-3xl'><HiOutlineDocumentDownload/></span></div>
                         </a>
                     </div>
                 </div>
