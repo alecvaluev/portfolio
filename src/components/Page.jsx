8eeb3511f3844b8a20c3e56072cd3dd20935be6e
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-24 13:40:10 -0400

    input dark mode color corrected

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -58,6 +58,9 @@ export default function Page({headerOpen}) {
     },
     '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
       color: 'white'
+    },
+    '& .css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input': {
+      color: 'white'
     }
     
   }
