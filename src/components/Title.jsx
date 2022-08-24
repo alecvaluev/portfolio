f1addc6450a9188aeda98c3db4027781534f9035
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-23 22:08:46 -0400

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { screenMode } from '../data/constants';
 
 export default function Title({title, subtitle, small = false}){
     return (
         <div className="flex flex-col items-center md:block font-bold mb-10">
             <h1 className={`${small? 'text-2xl' : 'text-5xl' } mb-2 capitalize`}>{title}</h1>
-            <div className="text-blue-500 uppercase">{subtitle}</div>
+            <div className={`${screenMode.highlight.text} uppercase`}>{subtitle}</div>
           </div>
     )
 }
