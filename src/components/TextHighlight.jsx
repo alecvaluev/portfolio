39095e411b86f0b1ab8477fe9c398dd457840080
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-23 22:08:30 -0400

    Update TextHighlight.jsx

--- src/components/TextHighlight.jsx
+++ src/components/TextHighlight.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { screenMode } from '../data/constants';
 
 export default function TextHighlight({name, text}){
     return (
         <div className="mb-2 font-bold text-gray-600">
-              <span className="text-blue-500 uppercase">{name} |</span> {text}
+              <span className={`${screenMode.highlight.text} uppercase`}>{name} |</span> {text}
             </div>
     )
 }
