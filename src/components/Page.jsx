a558f76a5a757dc9e0762026a246dcc32ad7f341
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-24 13:45:00 -0400

    Update Page.jsx

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -59,7 +59,7 @@ export default function Page({headerOpen}) {
     '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
       color: 'white'
     },
-    '& .css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input': {
+    '& .css-1sqnrkk-MuiInputBase-input-MuiOutlinedInput-input & .css-u36398': {
       color: 'white'
     }
     
