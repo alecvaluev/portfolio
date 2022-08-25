de622e154000eb0bc0f660058569c86c10633598
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-25 16:58:20 -0400

    replace images with videos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,60 +1,55 @@
-import vidtalkie_pic from '../images/vidtalkie.png';
-import path_pic from '../images/path-search.png';
-import sort_pic from '../images/sorting.png';
+import path_vid from '../images/pathsearch-vid.mp4';
+import sort_vid from '../images/sorting-vid.mp4';
+import vidtalkie_vid from '../images/vidtalkie-vid.mp4';
 
 const projects = [
     {
         prj_id: 1,
-        image: sort_pic,
+        video: sort_vid,
         github: 'https://github.com/alecvaluev/sort-algorithm-visualiser',
         website: 'https://sort-algorithm-visualiser.herokuapp.com',
         tools: ['ReactJs', 'ReduxJs', 'Bootstrap5']
     },
     {
         prj_id: 2,
-        image: path_pic,
+        video: path_vid,
         github: 'https://github.com/alecvaluev/path-search-visualiser',
         website: 'https://path-finding-visualiser-app.herokuapp.com/',
         tools: ['ReactJs', 'ReduxJs', 'Bootstrap5']
     },
     {
         prj_id: 3,
-        image: vidtalkie_pic,
+        video: vidtalkie_vid,
         github: 'https://github.com/alecvaluev/vidtalkie',
         website: 'https://vidtalkie.vercel.app',
         tools: ['ReactJs', 'TypeScript', 'NextJs', 'Tailwind CSS', '']
     },
     {
         prj_id: 4,
-        image: '',
         github: 'https://github.com/alecvaluev/Grocery-Store-Manager',
         website: '',
         tools: ['C']
     },
     {
         prj_id: 5,
-        image: '',
         github: 'https://github.com/alecvaluev/mathGame',
         website: '',
         tools: ['C']
     },
     {
         prj_id: 6,
-        image: '',
         github: 'https://github.com/alecvaluev/company-web-app',
         website: '',
         tools: ['Express', 'Mongoose', 'bcryptjs']
     },
     {
         prj_id: 7,
-        image: '',
         github: 'https://github.com/alecvaluev/General-Hospital-Pre-Triage-Application',
         website: '',
         tools: ['C++']
     },
     {
         prj_id: 8,
-        image: '',
         github: 'https://github.com/alecvaluev/assembly-line-simulation',
         website: '',
         tools: ['C++']
