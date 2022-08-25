fb913787d713445536bea336b70b52d2e16fdd2f
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-25 16:57:14 -0400

    replaced images with videos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -10,12 +10,12 @@ import { FaExternalLinkAlt } from 'react-icons/fa';
 //import constants
 import { screenMode } from '../data/constants';
 
-export default function ProjectCard({name, description, image, github, website, tools}) {
+export default function ProjectCard({name, description, video, github, website, tools}) {
     const darkMode = useSelector(selectDarkMode);
     
     return (
-        <div className={`${image ? 'md:w-full': 'md:w-[33%]'} flex`}>
-            <div className={`rounded-xl m-2 shadow-lg ${screenMode.highlight.shadow} ${image && 'md:w-[50%] md:shadow-none'}`}
+        <div className={`${video ? 'md:w-full': 'md:w-[33%]'} flex`}>
+            <div className={`rounded-xl m-2 shadow-lg ${screenMode.highlight.shadow} ${video && 'md:w-[50%] md:shadow-none'}`}
                  style={{zIndex: 1000}}>
                 <div className ='p-5'>
                     <div className={`flex gap-3 font-bold ${screenMode.highlight.text} text-2xl mb-3 capitalize`}>
@@ -23,7 +23,7 @@ export default function ProjectCard({name, description, image, github, website, 
                         {name}
                     </div>
                     <div className='rounded overflow-hidden'>
-                        <div className={`${image && `${screenMode.highlight.md.bg}`} max-h-40 p-5`}>
+                        <div className={`${video && `${screenMode.highlight.md.bg}`} max-h-40 p-5`}>
                             <p className='text-ellipsis overflow-hidden'>{description}</p>
                         </div>
                     </div>
@@ -32,7 +32,7 @@ export default function ProjectCard({name, description, image, github, website, 
                     <div className={`py-3 border-t-4 border-b-4 inline-block ${darkMode? screenMode.dark.bg: screenMode.light.bg}`}>
                         <TextHighlight name={'Built with'} text={tools.toString().replaceAll(',', '  ')} />
                     </div>
-                    <div className={`flex flex-wrap justify-around ${image && 'md:justify-start'} font-semibold m-3`}>
+                    <div className={`flex flex-wrap justify-around ${video && 'md:justify-start'} font-semibold m-3`}>
                         {
                             github && <a href={github} className={`${screenMode.highlight.hover.text} border ${darkMode ? screenMode.dark.border: screenMode.light.border} rounded p-3 hover:border ${screenMode.highlight.hover.border} flex gap-2`}><span className='text-2xl'><AiOutlineGithub/></span> GitHub</a>
                         }
@@ -43,14 +43,16 @@ export default function ProjectCard({name, description, image, github, website, 
                 </div>
             </div>
             {
-                image && (
+                video && (
                     <div className='hidden md:flex place-items-center grayscale hover:grayscale-0 hover:ease-in-out hover:duration-300' 
                         style={{marginLeft: '-60px'}}
                         >
-                        <img src={image}
+                        <video src={video}
                             alt=''
                             className='h-[250px]'
                             style={{objectFit: 'cover'}}
+                            autoplay
+                            loop
                          />
                     </div>
                 )
