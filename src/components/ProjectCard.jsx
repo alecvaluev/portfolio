0b216f29e0766b5ddb0365f79fb3e0d8a89a69a6
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-26 02:35:38 -0400

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -18,19 +18,19 @@ export default function ProjectCard({prj}) {
     const { name, description, video, github, website, tools, completed } = prj;
 
     return (
-        <div className={`w-full ${!video && 'md:w-[30%]'} flex`}>
-            <div className={` ${screenMode.highlight.shadow} ${video ? 'border-t md:border-none md:w-[50%] md:shadow-none': 'rounded-xl m-2 shadow-lg'}`}
+        <div className={`w-full ${!video && 'md:w-[35%]'} flex`}>
+            <div className={`w-[100%] ${screenMode.highlight.shadow} ${video ? 'border-t md:border-none md:w-[50%] md:shadow-none': 'rounded-xl m-2 shadow-lg p-3'}`}
                  style={{zIndex: 1000}}>
                 <div>
-                    <div className={`flex gap-3 font-bold ${screenMode.highlight.text} text-5xl mt-10 uppercase`}>
+                    <div className={`flex gap-3 ${video ? 'font-bold text-5xl mt-8' : 'text-2xl'} ${screenMode.highlight.text} uppercase`}>
                         <span className='hidden md:block text-4xl'><AiOutlineFolder /></span>
                         {name}
                     </div>
-                    <div className='my-5'><span className='capitalize'>{language.completed}</span> {completed}</div>
+                    <div className={`${video && 'my-3'}`}><span className='capitalize'>{language.completed}</span> {completed}</div>
                     {
                         video && (
                             <>
-                                <div className='md:hidden my-10'>
+                                <div className='md:hidden my-8'>
                                     <video src={video}
                                         alt=''
                                         className='w-full'
@@ -51,12 +51,12 @@ export default function ProjectCard({prj}) {
                     
                 </div>
                 <div>
-                    <div className={`py-8 inline-block ${darkMode? screenMode.dark.bg: screenMode.light.bg}`}>
+                    <div className={`${video ? 'py-6' : 'py-2'} inline-block ${darkMode? screenMode.dark.bg: screenMode.light.bg}`}>
                         <TextHighlight name={'Built with'} text={tools.toString().replaceAll(',', '  ')}/>
                     </div>
-                    <div className={`flex flex-wrap justify-around ${video && 'md:justify-start'} font-semibold mx-3 my-10`}>
+                    <div className={`flex flex-wrap justify-around ${video ? 'md:justify-start md:my-4': 'md:my-0'} font-semibold mx-3 my-8`}>
                         {
-                            github && <a href={github} className={`${screenMode.highlight.hover.text} border ${darkMode ? screenMode.dark.border: screenMode.light.border} rounded p-3 hover:border ${screenMode.highlight.hover.border} flex gap-2`}><span className='text-2xl'><AiOutlineGithub/></span> GitHub</a>
+                            github && <a href={github} className={`${screenMode.highlight.hover.text} border ${darkMode ? screenMode.dark.border: screenMode.light.border} rounded p-3 hover:border ${screenMode.highlight.hover.border} flex gap-2`}><span className='text-2xl'><AiOutlineGithub/></span> <span className=''>GitHub</span></a>
                         }
                         {
                             website && <a href={website} className={`${screenMode.highlight.hover.text} border ${darkMode ? screenMode.dark.border: screenMode.light.border} rounded p-3 hover:border ${screenMode.highlight.hover.border} flex gap-2`}><span className='text-2xl'><FaExternalLinkAlt/></span> Web App</a>
