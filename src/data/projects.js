0b861e95a15a84af193386e2c882dcd920c1c1dd
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-26 01:21:33 -0400

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,3 @@
-import vidtalkie_pic from '../images/vidtalkie.png';
-import path_pic from '../images/path-search.png';
-import sort_pic from '../images/sorting.png';
-
 import path_vid from '../images/pathsearch-vid.mp4';
 import sort_vid from '../images/sorting-vid.mp4';
 import vidtalkie_vid from '../images/vidtalkie-vid.mp4';
@@ -9,7 +5,6 @@ import vidtalkie_vid from '../images/vidtalkie-vid.mp4';
 const projects = [
     {
         prj_id: 1,
-        image: sort_pic,
         video: sort_vid,
         github: 'https://github.com/alecvaluev/sort-algorithm-visualiser',
         website: 'https://sort-algorithm-visualiser.herokuapp.com',
@@ -18,7 +13,6 @@ const projects = [
     },
     {
         prj_id: 2,
-        image: path_pic,
         video: path_vid,
         github: 'https://github.com/alecvaluev/path-search-visualiser',
         website: 'https://path-finding-visualiser-app.herokuapp.com/',
@@ -27,7 +21,6 @@ const projects = [
     },
     {
         prj_id: 3,
-        image: vidtalkie_pic,
         video: vidtalkie_vid,
         github: 'https://github.com/alecvaluev/vidtalkie',
         website: 'https://vidtalkie.vercel.app',
@@ -36,7 +29,6 @@ const projects = [
     },
     {
         prj_id: 4,
-        image: '',
         github: 'https://github.com/alecvaluev/Grocery-Store-Manager',
         website: '',
         tools: ['C'],
@@ -44,7 +36,6 @@ const projects = [
     },
     {
         prj_id: 5,
-        image: '',
         github: 'https://github.com/alecvaluev/mathGame',
         website: '',
         tools: ['C'],
@@ -52,7 +43,6 @@ const projects = [
     },
     {
         prj_id: 6,
-        image: '',
         github: 'https://github.com/alecvaluev/company-web-app',
         website: '',
         tools: ['Express', 'Mongoose', 'bcryptjs'],
@@ -60,7 +50,6 @@ const projects = [
     },
     {
         prj_id: 7,
-        image: '',
         github: 'https://github.com/alecvaluev/General-Hospital-Pre-Triage-Application',
         website: '',
         tools: ['C++'],
@@ -68,7 +57,6 @@ const projects = [
     },
     {
         prj_id: 8,
-        image: '',
         github: 'https://github.com/alecvaluev/assembly-line-simulation',
         website: '',
         tools: ['C++'],
