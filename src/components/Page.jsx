fe280456794c4ba57df078a1e7534c6a1a3d2d2f
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-26 16:12:48 -0400

    added animejs

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -6,6 +6,7 @@ import { selectProjects } from '../features/projects/projectsSlice';
 import { selectDarkMode } from "../features/darkMode/darkModeSlice";
 //import others
 import emailjs from '@emailjs/browser';
+import ReactAnime from 'react-animejs';
 //import components
 import Header from "./Header";
 import ProjectCard from "./ProjectCard";
@@ -16,7 +17,6 @@ import EducationCard from "./EducationCard";
 import LinksBar from "./LinksBar";
 //import React UI materials, icons, images
 import { TextField, Button } from "@mui/material";
-import { styled } from '@mui/material/styles';
 //import data
 import photo from '../images/photoshop.jpg';
 import { screenMode, programming_languages, web_technologies, databases} from '../data/constants';
@@ -27,6 +27,7 @@ export default function Page({headerOpen}) {
   const projects = useSelector(selectProjects);
   const darkMode = useSelector(selectDarkMode);
   const form = useRef();
+  const { Anime } = ReactAnime;
   
   const sendEmail = (e) => {
     e.preventDefault();
@@ -72,35 +73,101 @@ export default function Page({headerOpen}) {
   return (
     language && 
     <div className={`${darkMode? `${screenMode.dark.bg} ${screenMode.dark.text}` :screenMode.light.bg}`}>
-      <Header headerOpen={headerOpen}/> 
       
+      <Anime initial={[
+                  {
+                    targets: '#header',
+                    opacity: 1,
+                    translateX: 0,
+                    easing: "easeInOutQuad",
+                    duration: 2000,
+                    delay: 1700
+                  }
+      ]}>
+        <Header headerOpen={headerOpen}/> 
+      </Anime>
       <div>
         <div className="p-3 md:pt-44 md:px-40 xl:px-72">
 
-          <div className="flex flex-col md:flex-row items-center mt-10">
+          <div className="flex flex-col md:flex-row items-center mt-16 mb-36">
             <div className="w-[70%]">
-              <Title title={language.full_name} subtitle={language.job_position}/>
-              <div className="py-5">{language.about_me}</div>
-              <a href="#contact">
-                <Button variant="outlined" >&lt; {language.contact_title} /&gt;</Button>
-              </a>
+              <div className="flex flex-col items-center md:block font-bold mb-10">
+                <Anime initial={[
+                  {
+                    targets: ['#full_name', '#job_position'],
+                    translateX: 0,
+                    translateY: 0,
+                    opacity: 1,
+                    easing: "easeInOutQuad",
+                    duration: 1700
+                    
+                  }
+                ]}>
+                  <h1 id='full_name' 
+                      className='text-5xl mb-2 capitalize' 
+                      style={{opacity: 0, transform: 'translateX(-200px)'}}
+                      >
+                        {language.full_name}
+                    </h1>
+                  <div id='job_position' 
+                       className={`${screenMode.highlight.text} uppercase`}
+                       style={{opacity: 0, transform: 'translateY(100px)'}}
+                       >
+                        {language.job_position}
+                  </div>
+                </Anime>
+              </div>
+        
+              <Anime initial={[
+                  {
+                    targets: ['#about_me'],
+                    opacity: 1,
+                    easing: "easeInOutQuad",
+                    duration: 2000,
+                    delay: 1700
+                    
+                  }
+                ]}>
+                  <div id='about_me'
+                       style={{opacity: 0}}
+                    >
+                    <div className="py-5">{language.about_me}</div>
+                    <a href="#contact">
+                      <Button variant="outlined" >&lt; {language.contact_title} /&gt;</Button>
+                    </a>
+                  </div>
+              </Anime>
             </div>
            
-           <div className={`rounded-xl ${screenMode.highlight.bg} -rotate-3 h-[250px] w-[200px] my-10`}>
-            <div className="rounded-xl overflow-hidden rotate-10 h-[250px]">
-                <img src={photo}
-                    alt={`${language.full_name} - ${language.job_position}`}
-                    className='w-[200px]'
-                      />
+            <Anime initial={[
+                  {
+                    targets: '#photo',
+                    opacity: 1,
+                    easing: "easeInOutQuad",
+                    duration: 2000,
+                    delay: 1700
+                  }
+            ]}>
+              <div id='photo' 
+                   className={`rounded-xl ${screenMode.highlight.bg} -rotate-3 h-[250px] w-[200px] my-10`}
+                   style={{opacity: 0}}>
+                <div className="rounded-xl overflow-hidden rotate-10 h-[250px]">
+                    <img src={photo}
+                        alt={`${language.full_name} - ${language.job_position}`}
+                        className='w-[200px]'
+                          />
+                  </div>
               </div>
-           </div>
+            </Anime>
 
           </div>
 
           {/* about */}
-          <div id="about" className="py-12 border-y">
+          <div id="about" className="py-20 border-y">
             <Title title={language.about_title} subtitle={language.about_subtitle}/>
             <div className="my-5">{language.about_description}</div>
+            <div className="my-5">{language.about_description2}</div>
+            <div className="my-5">{language.about_description3}</div>
 
             <TextHighlight name={language.headline_live} text={language.current_address}/>
             <TextHighlight name={language.headline_email} text={'alexvaluev1220@gmail.com'}/>
@@ -108,13 +175,13 @@ export default function Page({headerOpen}) {
           </div>
 
           {/* education */}
-          <div id='education' className="py-12 border-y">
+          <div id='education' className="py-20 border-y">
             <Title title={language.education_title} subtitle={language.education_subtitle}/>
             <EducationCard startDate={language.program_start} endDate={language.program_end} program={language.university_program} uni_name={language.university_name} uni_address={language.university_address} />
           </div>
 
           {/* skills */}
-          <div id='skills' className="py-12 border-y">
+          <div id='skills' className="py-20 border-y">
             {/* title */}
             <Title title={ language.skills_title} subtitle={ language.skills_subtitle} />
             {/* content */}
@@ -126,7 +193,7 @@ export default function Page({headerOpen}) {
           </div>
 
           {/* projects */}
-          <div id='projects' className="py-12 border-y">
+          <div id='projects' className="py-20 border-y">
             {/* title */}
             <Title title={language.project_title} subtitle={language.project_subtitle}/>
             {/* content */}
@@ -151,7 +218,7 @@ export default function Page({headerOpen}) {
           </div>
 
           {/* contact */}
-          <div id='contact' className="py-12 border-y">
+          <div id='contact' className="py-20  border-y">
             <Title title={ language.contact_title} subtitle={ language.contact_subtitle} />
             {/* content form */}
             <div className="md:flex md:flex-row-reverse md:justify-around md:mr-6">
