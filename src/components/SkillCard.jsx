9ca441b2e827e3288a125241722aa58d7bfd7ff2
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-26 12:53:33 -0400

    Update SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { screenMode } from '../data/constants';
 
 export default function SkillCard({name, list}){
     return (
-        <div className=" rounded-2xl m-3 w-[90%] md:w-[30%] shadow-lg shadow-blue-500/50">
-            <div className="font-bold text-xl py-4 bg-blue-500 rounded-2xl capitalize text-center">{name}</div>
+        <div className={`rounded-2xl m-3 w-[70%] md:w-[30%] shadow-lg ${screenMode.highlight.shadow}`}>
+            <div className={`font-bold text-xl py-4 ${screenMode.highlight.bg} rounded-2xl capitalize text-center`}>{name}</div>
             <div className="p-6 font-semibold text-gray-500 flex flex-col gap-1 text-center md:text-left">
                 {
                     list.map((item, idx) => <div key={idx}>{item}</div>)
