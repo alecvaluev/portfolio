70da4989d18f76b312014a9088ffbf9e6605c00b
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-09-29 17:20:34 -0400

    Update projectsSlice.js

--- src/features/projects/projectsSlice.js
+++ src/features/projects/projectsSlice.js
@@ -24,16 +24,11 @@ export const selectProjects = (state) => {
     const prjCopy = JSON.parse(JSON.stringify(state.projects));
 
     return prjCopy.map(prj => { 
-        const language = projectLanguage.find(lang => prj.prj_id === lang.prj_id);
-        if (!language) 
-            {
-            for(let key of Object.keys(projectEmptyTemplate)){
+        let language = projectLanguage.find(lang => prj.prj_id === lang.prj_id);
+        if (!language) language = projectEmptyTemplate;
+        
+        for(let key of Object.keys(language)){
             prj[key] = language[key];
-            }
-        else{
-            for(let key of Object.keys(language)){
-            prj[key] = language[key];
-            }
         }
         return prj;
     })
